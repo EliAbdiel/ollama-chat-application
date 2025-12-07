7ef928543b4281d6e951aee5e02689363147271f
EliAbdiel <371.abdiel@gmail.com>
2025-12-07 14:27:06 -0500

    feat: COMMAND loader to config.py – centralizes settings

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,8 +6,11 @@ load_dotenv()
 COMMANDS = {"Search", "Chat", "Summary"}
 
 OLLAMA_API_KEY=os.environ["OLLAMA_SECRET_KEY"]
+
 OLLAMA_BASE_URL=os.environ["OLLAMA_BASE_URL"]
+
 DEFAULT_MODEL=os.environ["DEFAULT_MODEL"]
+
 VISION_MODEL=os.environ["VISION_MODEL"]
 
 ELEVENLABS_KEY=os.environ["ELEVENLABS_API_KEY"]
@@ -15,5 +18,7 @@ ELEVENLABS_KEY=os.environ["ELEVENLABS_API_KEY"]
 DATABASE=os.environ["LOCAL_DATABASE"]
 
 CONTAINER = os.environ["CONTAINER_NAME"]
+
 STORAGE_ACCOUNT = os.environ["STORAGE_ACCOUNT_NAME"]
+
 STORAGE_SECRET = os.environ["STORAGE_KEY"]
