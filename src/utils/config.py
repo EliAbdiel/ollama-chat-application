b7d8da4a432dde723b5fe814b919d450d7aaa7f9
EliAbdiel <371.abdiel@gmail.com>
2026-04-20 01:00:49 -0500

    feat: update environment config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,24 +1,22 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 COMMANDS = {"Search", "Chat", "Summary"}
 
-OLLAMA_API_KEY=os.environ["OLLAMA_SECRET_KEY"]
+OLLAMA_API_KEY=os.environ.get("OLLAMA_SECRET_KEY")
 
-OLLAMA_BASE_URL=os.environ["OLLAMA_BASE_URL"]
+OLLAMA_BASE_URL=os.environ.get("OLLAMA_BASE_URL")
 
-DEFAULT_MODEL=os.environ["DEFAULT_MODEL"]
+DEFAULT_MODEL=os.environ.get("DEFAULT_MODEL")
 
-VISION_MODEL=os.environ["VISION_MODEL"]
+VISION_MODEL=os.environ.get("VISION_MODEL")
 
-ELEVENLABS_KEY=os.environ["ELEVENLABS_API_KEY"]
+ELEVENLABS_KEY=os.environ.get("ELEVENLABS_API_KEY")
 
-DATABASE=os.environ["LOCAL_DATABASE"]
+DATABASE=os.environ.get("LOCAL_DATABASE")
 
-CONTAINER = os.environ["CONTAINER_NAME"]
+CONTAINER = os.environ.get("CONTAINER_NAME")
 
-STORAGE_ACCOUNT = os.environ["STORAGE_ACCOUNT_NAME"]
+STORAGE_ACCOUNT = os.environ.get("STORAGE_ACCOUNT_NAME")
 
-STORAGE_SECRET = os.environ["STORAGE_KEY"]
+STORAGE_SECRET = os.environ.get("STORAGE_KEY")
